29c57ea09f9dbc36b1ec2b4180206fa5d66fd3fe
Toni Anne Romero <tonianne.romero@gmail.com>
2015-12-08 19:10:10 +0800

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.delete_all
+Event.create([
+  {title: "Barbeque"},
+  {title: "Barbeque"}
+])
+
+Task.delete_all
+Task.create([
+  {
+    title: "Bring Speakers"
+    description: "small speakers -toni"
+  },
+  {
+    title: "Bring Speakers"
+    description: "small speakers -toni"
+  },
+])
